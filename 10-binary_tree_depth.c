5f1fe9cc50504b1e6c8fe27a6417d7747de83e1e
foash-111 <mr2221@fayoum.edu.eg>
2024-02-29 17:56:03 +0200

    binary_tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,18 +8,18 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
-    const binary_tree_t *current;
+	size_t depth = 0;
+	const binary_tree_t *current;
 
-    if (tree == NULL)
-    return (0);
+	if (tree == NULL)
+	return (0);
 
-    current = tree;
+	current = tree;
 
-    while (current->parent)
-    {
-        depth += 1;
-        current = current->parent;
-    }
-    return (depth);
+	while (current->parent)
+	{
+		depth += 1;
+		current = current->parent;
+	}
+	return (depth);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,33 +8,35 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_sum = 0;
-    size_t right_sum = 0;
+	size_t left_sum = 0;
+	size_t right_sum = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    
-    if (tree->left)
-    {
-    left_sum += binary_tree_height(tree->left) + 1;
-    // I used if to ensure that left_child is exist so,
-    // increase left_sum by 1 safely
-    // if i didn't use if, it will increase left sum by 1 even if left child -
-    // is null
-    }
-    if (tree->right)
-    {
-    right_sum += binary_tree_height(tree->right) + 1;
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+	left_sum += binary_tree_height(tree->left) + 1;
+	/**
+	* I used if to ensure that left_child is exist so,
+	* increase left_sum by 1 safely
+	* if i didn't use if, it will increase left sum by 1 even if left child -
+	* is null
+	*/
+	}
+	if (tree->right)
+	{
+	right_sum += binary_tree_height(tree->right) + 1;
+	}
 
 
 
 
-    if (left_sum >= right_sum)
-    return (left_sum);
-    else
-    return (right_sum);
+	if (left_sum >= right_sum)
+	return (left_sum);
+	else
+	return (right_sum);
 
 }
