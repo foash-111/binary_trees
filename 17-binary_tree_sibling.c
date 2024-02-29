1d981f61f48e8ef225609435ef36e7e648ed0c5a
foash-111 <mr2221@fayoum.edu.eg>
2024-02-29 23:57:09 +0200

    siblings

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - This is a tree in which every node has-
- * either zero children or two children. 
- * @tree: pointer to the node that we want to calc the height from
+ * binary_tree_sibling - This is a tree in which every node has-
+ * either zero children or two children.
+ * @node: pointer to the node that we want to calc the height from
  * Return: pointer to the sibling node
 */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (
-        node == NULL || node->parent == NULL ||
-        node->parent->left == NULL || node->parent->right == NULL
-        )
-    return (NULL);
+	if (
+		node == NULL || node->parent == NULL ||
+		node->parent->left == NULL || node->parent->right == NULL
+		)
+	return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 
 
 
