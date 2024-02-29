8fb18b64ba073e8157a95a15437dd53b970b1be3
foash-111 <mr2221@fayoum.edu.eg>
2024-02-29 17:08:59 +0200

    binary_tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,6 +16,6 @@ int binary_tree_balance(const binary_tree_t *tree)
         return (0);
     }
     
-    
+
     return ((binary_tree_height(tree->left))) - (( binary_tree_height(tree->right)));
 }
