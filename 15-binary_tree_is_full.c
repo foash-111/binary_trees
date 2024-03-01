1de546d2f810f08225f1b9bfd7180429b30e1cd6
foash-111 <mr2221@fayoum.edu.eg>
2024-03-01 13:03:08 +0200

    check the tree if it full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_full - This is a tree in which every node has-
- * either zero children or two children. 
+ * either zero children or two children.
  * @tree: pointer to the node that we want to calc the height from
  * Return: he height of the subtree rooted at its left child minus--
  * the height of the subtree rooted at its right child
@@ -10,12 +10,24 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-    if ((tree->left && tree->right) || ((tree->left == NULL && tree->right == NULL)))
-    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	if (
+		(tree->left == NULL && tree->right) ||
+		(tree->right == NULL && tree->left)
+		)
+	{
+		return (0);
+	}
 
-    return (0);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+	return (1);
+	}
+	return  ((binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right)));
+
+}
