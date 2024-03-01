6d363baf7c1772f850f58280cda90a0c152ebf4c
foash-111 <mr2221@fayoum.edu.eg>
2024-03-02 01:33:37 +0200

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,13 +13,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (
-		(tree->left == NULL && tree->right) ||
-		(tree->right == NULL && tree->left)
-		)
-	{
-		return (1);
-	}
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
@@ -30,7 +23,5 @@ if (binary_tree_height(tree->left) >= binary_tree_height(tree->right))
 return (binary_tree_height(tree->left) + 1);
 
 return (binary_tree_height(tree->right) + 1);
-
-
 
 }
