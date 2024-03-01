80f8b803a68cc07ad5d577301e4f2ab66b039bba
foash-111 <mr2221@fayoum.edu.eg>
2024-03-01 13:37:07 +0200

    check the tree if it perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - all leaf nodes are at the max depth of the tree,
+ *  and the tree is completely filled with no gaps.
+ * @tree: pointer to the node that we want to calc the height from
+ * Return: 1 if it is perfect, 0 otherwise
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (
+		(tree->left == NULL && tree->right) ||
+		(tree->right == NULL && tree->left)
+		)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+	return (1);
+	}
+
+if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+return (0);
+
+
+return (binary_tree_height(tree->left) * binary_tree_height(tree->right));
+}
