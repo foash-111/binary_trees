e038df1429474ae46d04db8560e17a5768f62bd7
foash-111 <mr2221@fayoum.edu.eg>
2024-03-01 13:04:23 +0200

    check the tree if it full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -28,6 +28,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 	return (1);
 	}
-	return  ((binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right)));
+return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
 
 }
