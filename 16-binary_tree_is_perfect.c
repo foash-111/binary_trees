388b14a76c8b52957996210424408004e11368b1
foash-111 <mr2221@fayoum.edu.eg>
2024-03-01 13:43:12 +0200

    check the tree if it perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,42 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height_helper - binary_tree_height
+ * @tree: pointer to the node that we want to calc the height from
+ * Return: the height of the tree..("node")
+*/
+
+size_t tree_height_helper(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (
+		(tree->left == NULL && tree->right) ||
+		(tree->right == NULL && tree->left)
+		)
+	{
+		return (1);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+	return (0);
+	}
+
+if (tree_height_helper(tree->left) >= tree_height_helper(tree->right))
+return (tree_height_helper(tree->left) + 1);
+
+return (tree_height_helper(tree->right) + 1);
+
+}
+
+
+
+
 /**
  * binary_tree_is_perfect - all leaf nodes are at the max depth of the tree,
  *  and the tree is completely filled with no gaps.
@@ -28,9 +65,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (1);
 	}
 
-if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+if (tree_height_helper(tree->left) != tree_height_helper(tree->right))
 return (0);
 
 
-return (binary_tree_height(tree->left) * binary_tree_height(tree->right));
+return (tree_height_helper(tree->left) * tree_height_helper(tree->right));
 }
